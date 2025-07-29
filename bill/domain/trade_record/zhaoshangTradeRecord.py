94e3f777f6d6ebe8988c17de488e2c33b2b2d1e7
chLemon <550125499@qq.com>
2025-07-29 12:46:57 +0800

    update

--- bill/domain/trade_record/zhaoshangTradeRecord.py
+++ bill/domain/trade_record/zhaoshangTradeRecord.py
@@ -4,10 +4,10 @@ from pydantic import Field
 
 class PufaCreditTradeRecord(BaseTradeRecord):
     """
-    浦发银行信用卡交易记录类
+    招商银行交易记录类
 
-    交易日期 记账日期 交易摘要 卡号末四位 交易金额
-    20230101 20230101 财付通-黄焖鸡米饭 1234 ￥20.0
+    记账日期 货币 交易金额 联机余额 交易摘要 对手信息
+    2024-01-01 CNY 1.00 100.00 快捷支付 微信转账
 
     Attributes:
         extends from BaseTradeRecord:
